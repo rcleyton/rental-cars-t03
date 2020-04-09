e827388feb0e75428ad8d86be98dc9d0099fd577
rcleyton <silva.rcleyton@gmail.com>
2020-04-09 18:25:15 -0300

    Admin register manufacturer

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -6,5 +6,20 @@ class ManufacturersController < ApplicationController
   def show
     @manufacturer = Manufacturer.find(params[:id])
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    @manufacturer.save
+    redirect_to @manufacturer
+  end
+
+  private
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
+  end
   
 end
