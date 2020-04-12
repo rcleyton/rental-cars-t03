a7e24ecc645a943858cf591c5dab774f936ad46b
rcleyton <silva.rcleyton@gmail.com>
2020-04-12 19:58:53 -0300

    Admin register subsidiary

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -6,4 +6,20 @@ class SubsidiariesController  < ApplicationController
   def show
     @subsidiary = Subsidiary.find(params[:id])
   end
+
+  def new
+    @subsidiary = Subsidiary.new
+  end
+
+  def create
+    @subsidiary = Subsidiary.new(subsidiary_params)
+    @subsidiary.save
+    redirect_to @subsidiary
+  end
+
+  private
+  def subsidiary_params
+    params.require(:subsidiary).permit(:name, :cnpj, :adress)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :manufacturers, only: [:index, :new, :show, :create]
-  resources :subsidiaries, only: [:index, :show]
+  resources :subsidiaries, only: [:index, :new, :show, :create]
   
 end
