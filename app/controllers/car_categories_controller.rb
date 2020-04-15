670d9fe872cef988bbfbd84bb0973e76b74238a3
rcleyton <silva.rcleyton@gmail.com>
2020-04-15 00:23:16 -0300

    Admin register category

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -7,4 +7,19 @@ class CarCategoriesController < ApplicationController
     @car_category = CarCategory.find(params[:id])
   end
 
+  def new
+    @car_category = CarCategory.new
+  end
+
+  def create 
+    @car_category = CarCategory.new(car_category_params)
+    @car_category.save
+    redirect_to @car_category
+  end
+
+  private
+  def car_category_params
+    params.require(:car_category).permit(:name, :daily_rate, :car_insurance, 
+                   :third_party_insurance)
+  end
 end
---
+++ spec/features/admin_register_category_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'Admin register category' do
+  scenario 'from index page' do
+    visit root_path
+    click_on 'Categoria de carros'
+
+    expect(page).to have_link('Registrar nova categoria', href: new_car_category_path)
+  end
+
+  scenario 'successfully' do
+    visit root_path
+    click_on 'Categoria de carros'
+    click_on 'Registrar nova categoria'
+     
+    fill_in 'Nome', with: 'Intermediário'
+    fill_in 'Diária', with: 70
+    fill_in 'Seguro', with: 35
+    fill_in 'Seguro de terceiros', with: 21
+    click_on 'Enviar'
+
+    expect(current_path).to eq car_category_path(CarCategory.last.id)
+    expect(page).to have_content('Intermediário')
+    expect(page).to have_content(70)
+    expect(page).to have_content(35)
+    expect(page).to have_content(21)
+    expect(page).to have_link('Voltar')
+  end  
+  
+end
