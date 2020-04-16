fd19579ee58bbcaaed429ae568ba9698985c488c
rcleyton <silva.rcleyton@gmail.com>
2020-04-16 11:07:06 -0300

    Fixed admin view subsidiaries test

--- spec/features/admin_view_subsidiaries_spec.rb
+++ spec/features/admin_view_subsidiaries_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 feature 'admin view subsidiaries' do
   scenario 'successfully' do
-    Subsidiary.create!(name: 'Filial Paraná')
-    Subsidiary.create!(name: 'Filial Espírito Santo')
+    Subsidiary.create!(name: 'Minas Gerais')
+    Subsidiary.create!(name: 'São Paulo')
     
     visit root_path
     click_on 'Filiais'
 
-    expect(page).to have_content('Filial Paraná')
-    expect(page).to have_content('Filial Espírito Santo')
+    expect(page).to have_content('Minas Gerais')
+    expect(page).to have_content('São Paulo')
   end
 
   scenario 'and view details' do
-    Subsidiary.create!(name: 'Filial Paraná')
-    Subsidiary.create!(name: 'Filial Espírito Santo')
+    Subsidiary.create!(name: 'Minas Gerais', 
+                       cnpj: '07.694.743/0001-58',
+                       adress: 'Avenida do Contorno, 7777')
 
     visit root_path
     click_on 'Filiais'
-    click_on 'Filial Paraná'
+    click_on 'Minas Gerais'
 
-    expect(page).to have_content('Filial Paraná')
-    expect(page).not_to have_content('Filial Espírito Santo')
+    expect(page).to have_css('h1', text: 'Minas Gerais')
+    expect(page).to have_css('li', text: '07.694.743/0001-58')
+    expect(page).to have_css('li', text: 'Avenida do Contorno, 777')
   end
   
   scenario 'and no subsidiaries are created' do
