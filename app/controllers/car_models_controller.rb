5529b5bc863ac1b9dc5439d67c2ecaae6af89a81
rcleyton <silva.rcleyton@gmail.com>
2020-05-03 16:17:24 -0300

    Register car model

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -6,5 +6,23 @@ class CarModelsController < ApplicationController
   def show
     @car_model = CarModel.find(params[:id])
   end
+
+  def new
+    @car_model = CarModel.new
+    @manufactures = Manufacturer.all
+    @car_categories = CarCategory.all 
+  end
+
+  def create
+    @car_model = CarModel.new(car_model_params)
+    @car_model.save
+    redirect_to @car_model
+  end
+
+  private
+  def car_model_params
+    params.require(:car_model).permit(:name, :motorization, :year, :fuel_type,
+                                      :manufacturer_id, :car_category_id)
+  end
   
 end
--- spec/models/subsidiary_spec.rb
+++ spec/models/subsidiary_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 describe Subsidiary do
-  context '#name' do
-    it "name can't be blank" do
+  context '#name ' do
+    it "can't be blank" do
       subsidiary = Subsidiary.new(name: '')    
       subsidiary.valid?
       expect(subsidiary.errors[:name]).to include('não pode ficar em branco')
     end
 
     it 'must be unique' do
-      Subsidiary.create!(name: 'Paulista', cnpj: '75.541.502/0001-09')
+      Subsidiary.create!(name: 'Paulista', cnpj: '75.641.502/0001-09')
       subsidiary = Subsidiary.new(name: 'Paulista')
       subsidiary.valid?
       expect(subsidiary.errors[:name]).to include('já está em uso')
     end
   end
 
-  context '#cnpj' do
-    it 'cnpj cannot be blank' do
+  context '#cnpj ' do
+    it 'cannot be blank' do
       subsidiary = Subsidiary.new(cnpj: '')
       subsidiary.valid?
       expect(subsidiary.errors[:cnpj]).to include('não pode ficar em branco')
